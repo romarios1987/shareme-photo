980a893e7107c0082c89a83c865a42cdaed74748
romarios1987 <romios047@gmail.com>
2022-10-29 21:46:16 +0300

    Google auth & Login component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Login from './components/Login';
+import Home from './container/Home';
+
 const App = () => {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
   );
 };
 
